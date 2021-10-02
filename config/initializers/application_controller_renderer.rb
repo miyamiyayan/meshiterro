1fb549caafde4d82469f7b7cac831117f98b1833
miyamiyayan <dmkm.25.active@gmail.com>
2021-10-02 01:36:59 +0000

    [stert]4-0~3

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'eba4fdc7ab72eb87ad38d12193bff29611c4b449b191971174e3120a0e23baa6f3d07e8a15790705420cef84791f5d976efb0235b95f81cb56bd6d24873b9f35'
